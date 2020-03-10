fb101137963bedbf07d62838c6636b86fb230b1e
ilaan <benjailaan@gmail.com>
2020-03-10 19:02:50 +0300

    restoring second test

--- multiples.py
+++ multiples.py
@@ -1,7 +1,5 @@
-def multiple():
-    num = 1
-    while(num <= 101):
-        if(num % 4 == 0):
-            print(num)
-        num += 1
-multiple()
+num = 1
+while (num <= 101):
+    if(num % 4 == 0):
+        print(num)
+    num+=1
