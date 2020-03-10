efdf0b9905a85361b5ccb726100f9b8bba2480e7
ilaan <benjailaan@gmail.com>
2020-03-10 18:46:38 +0300

    third multiple test

--- multiples.py
+++ multiples.py
@@ -1,5 +1,7 @@
-num = 1
-while (num <= 101):
-    if(num % 4 == 0):
-        print(num)
-    num+=1
+def multiple():
+    num = 1
+    while(num <= 101):
+        if(num % 4 == 0):
+            print(num)
+        num += 1
+multiple()
