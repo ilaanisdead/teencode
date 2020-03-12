d9c5f260bfe83cff3fa98749164bc7144074e8a0
ilaan <benjailaan@gmail.com>
2020-03-12 11:20:56 +0300

    added main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+import random
+import urllib.request
+def download_web_image(url):
+    name = random.randrange(1, 100)
+    full_name = str(name) + ".jpg"
+    urllib.request.urlretrieve(url, full_name)
+download_web_image("https://static3.bigstockphoto.com/1/2/3/large2/321033145.jpg")
