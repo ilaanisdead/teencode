fa828e1afe5af9f7fec7b4c3664ae2ef80c28678
ilaan <benjailaan@gmail.com>
2020-05-04 13:36:28 +0300

    reversed chunks of list

---
+++ rev_chunk.py
@@ -0,0 +1,8 @@
+lst = [11, 45, 8, 23, 14, 12, 78, 45, 89 ]
+Chunk_1 = lst[0:3]
+Chunk_2 = lst[3:6]
+Chunk_3 = lst[6:9]
+print(Chunk_2)
+print(list(reversed(Chunk_1)))
+print(list(reversed(Chunk_2)))
+print(list(reversed(Chunk_3)))
