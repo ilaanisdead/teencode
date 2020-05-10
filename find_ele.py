19d659f49438179911f1898aa46bb49ebed52f91
ilaan <benjailaan@gmail.com>
2020-05-10 14:13:36 +0300

    finding single element

---
+++ find_ele.py
@@ -0,0 +1,9 @@
+def element(array):
+    
+    for x, v in enumerate(array):
+        if array.count(v) == 1:
+            return array[x]
+
+print(element([2,2,1]))
+print(element([2,2,1,1]))
+print(element([4,1,2,1,2]))
