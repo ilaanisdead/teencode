885fdfabc0c81402d22c3a984504cfb3b68655fd
ilaan <benjailaan@gmail.com>
2020-05-19 21:09:02 +0300

    prime_update

--- prime_num.py
+++ prime_num.py
@@ -1,9 +1,15 @@
+max = int(input("Find primes up to: "))
+primeList = []
 
-for num in range(1, 50):
-    if num > 1:
-        for i in range(2,num):
-            if(num % i) == 0:
-                break
-            else:
-                print(num)
+for x in range(2,max+1):
+    isPrime = True
+    for y in range(2, int(x ** 0.5) + 1):
+        if x % y == 0:
+            isPrime = False
+            break
+    if isPrime:
+        primeList.append(x)
+
+print(primeList)
+                
                 
