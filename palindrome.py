9b79021ff594cc590f921fa7c8c999990614e87c
ilaan <benjailaan@gmail.com>
2020-05-28 20:05:00 +0300

    palindrome

--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,10 @@
 def palin(word):
-    if type(word)== str:
-        for n in word:
-            if word == word[::-1]:
-                word = True
-            else:
-                word = False
-            return word
+    if type(word) != int:
+        if word == word[::-1]:
+            return True
+        return False
     else:
-        print("enter a word")
-
+        print('Enter a word')
 print(palin("madam"))
 print(palin(101))
+
